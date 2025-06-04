6a0eef2a72718b1c1bd1420df75b1ba146904cf4
github.com/nbox <gitnbox@gmail.com>
2025-06-04 20:33:09 +0300

    Initial Commit

---
+++ ObsidianHotCornerPreview/ObsidianHotCornerPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsidianHotCornerPreviewApp.swift
+//  ObsidianHotCornerPreview
+//
+//  Created by User on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ObsidianHotCornerPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObsidianHotCornerPreviewTests/ObsidianHotCornerPreviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObsidianHotCornerPreviewTests.swift
+//  ObsidianHotCornerPreviewTests
+//
+//  Created by User on 6/4/25.
+//
+
+import Testing
+@testable import ObsidianHotCornerPreview
+
+struct ObsidianHotCornerPreviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
